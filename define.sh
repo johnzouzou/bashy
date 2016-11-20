d35e4bb91cb070b292388c01550d510d3221a9ba
John Zouzounis <John Zouzounis>
2016-11-20 14:53:12 -0700

    define.sh defines a word

---
+++ define.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+define(){
+        t=$@
+        for words in t
+        do
+        someOther=$(curl "https://www.vocabulary.com/dictionary/$words" | grep '<p class        ="long">' || echo '\n')
+        echo $someOther
+        done
+}
+if [ $# -eq 0 ]
+then
+echo "enter something to define silly"
+else
+for words in $@
+do
+s=$(curl "https://www.vocabulary.com/dictionary/$words" | grep '<p class="long">') ;
+echo $s
+done
+fi
